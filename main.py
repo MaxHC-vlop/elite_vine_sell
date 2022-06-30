351d462e9097cdd9f41f95851526c4517cf80715
Vladislav Dragushin <maxhc@inbox.ru>
2022-06-30 16:22:21 +0300

    in process

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
