a36073617c5bfb055693f87f21f27394d0a82446
Vladuslav Dragushin <maxhc@inbox.ru>
2022-07-19 01:01:40 +0300

    task 9-12

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas
+
 from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
@@ -9,10 +11,25 @@ CREATE_YEAR = 1920
 
 def get_year():
     now_year = datetime.now().year
-
     now_time = now_year - CREATE_YEAR
+    inf_name = now_time % 10
+    inf_name2 = now_time % 100
+    isll2 = 1
+    iksl3 = (2, 3, 4)
+    plop = (11, 12, 13, 14)
+    if inf_name2 in plop:
+        return f'{now_time} лет'
+    elif inf_name == isll2:
+        return f'{now_time} год'
+    elif inf_name in iksl3:
+        return f'{now_time} года'
+    else:
+        return f'{now_time} лет'
 
-    return now_time
+
+def new_name():
+    excel_data_df = pandas.read_excel('wine.xlsx')
+    wine_dict = excel_data_df.to_dict()
 
 
 def make_template():
@@ -21,11 +38,28 @@ def make_template():
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
+    excel_data_df = pandas.read_excel('wine.xlsx')
+    wine_dict = excel_data_df.to_dict()
+
+    new_dict = []
+    for i in range(6):
+        img = wine_dict['Картинка'][i]
+        src = './images/'
+        x = {
+            'image': f'{src}{img}',
+            'title': wine_dict['Название'][i],
+            'sort': wine_dict['Сорт'][i],
+            'price': wine_dict['Цена'][i],
+        }
+        new_dict += [x]
+    print(new_dict)
     rendered_page = template.render(
-        old_wine = f'{get_year()}'
+        new_dict = new_dict,
+        old_wine = get_year()
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
+
 
 def main():
     make_template()
